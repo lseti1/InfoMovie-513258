04d2d5758074bb6309f2ca3d92534427f63ec8c1
Lucas Setiady <luca.setia@gmail.com>
2025-08-04 13:52:38 +1000

    Files: Set-up initial Angular setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class', // or 'media' to have darkMode functionality
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
