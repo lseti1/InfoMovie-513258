9f3537be29acbc94746552d34336e59a10c4307c
Lucas Setiady <luca.setia@gmail.com>
2025-08-04 15:51:05 +1000

    Functionality: Added functionality to free API for info display & added simple movie card display

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Tmdb } from './tmdb';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule],
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('angularApp');
-  count = signal(0);
 
-  increment() {
-    this.count.set(this.count() + 1);
+  movies: any[] = [];
+  query = '';
+
+  constructor(private tmdb: Tmdb) {}
+
+  search() {
+    if (!this.query.trim()) return;
+    this.tmdb.searchMovies(this.query).subscribe((response) => {
+      this.movies = response.results;
+    })
   }
 }
---
+++ src/app/tmdb.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.local';
+
+const API_KEY = environment.tmdbApiKey; 
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Tmdb {
+  constructor(private http: HttpClient) {}
+
+  searchMovies(query: string): Observable<any> {
+    return this.http.get(`${BASE_URL}/search/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: query
+      }
+    });
+  }
+
+  getMovieDetails(id: string): Observable<any> {
+    return this.http.get(`${BASE_URL}/movie/${id}`, {
+      params: {
+        api_key: API_KEY
+      }
+    });
+  }
+}
